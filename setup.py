52a59d2e44f216902f297254b09dde16d8541983
Max Tyler <maxastyler@gmail.com>
2021-02-16 10:16:27 +0000

    Add numpy version requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'numpy' ]
+requirements = [ 'numpy>=1.19' ]
 
 setup_requirements = ['pytest-runner', ]
 
