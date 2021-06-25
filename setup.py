03a977b87442cebfe7e1345243de1f77f59c464e
Max Tyler <maxastyler@gmail.com>
2021-06-25 16:48:27 +0100

    Modify setup

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """The setup script."""
 
 from setuptools import setup, find_packages
@@ -10,11 +9,15 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'numpy>=1.19', 'scipy>=1.5' ]
+requirements = ['numpy>=1.19', 'scipy>=1.5']
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    'pytest-runner',
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    'pytest>=3',
+] + requirements
 
 setup(
     author="Max Tyler",
